8e4180844802698efa325e1b5563276103f7231a
Alba serena <albaserenasuarez1998@gmail.com>
2020-02-20 09:36:36 +0100

    delete and add

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,4 +27,18 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero); //Lo elimina de la lista
+    this.heroService.deleteHero(hero).subscribe(); //Lo elimina del servidor
+  }
+
 }
